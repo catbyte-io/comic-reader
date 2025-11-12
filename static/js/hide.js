f5cb08425628c4e1f07d25ffa20f9e6071d194b6
catbyte-io <74119748+catbyte-io@users.noreply.github.com>
2025-11-12 16:46:25 -0700

    Add hiding animation for home logo button.

--- static/js/hide.js
+++ static/js/hide.js
@@ -1,9 +1,13 @@
 document.addEventListener('click', function() {
     const bannerDiv = document.getElementById('navBanner');
     bannerDiv.classList.remove('hide'); // Show the message
+    
+    const topDiv = document.getElementById('topBanner');
+    topDiv.classList.remove('hide');
 
     // Set a timer to hide the message after 3 seconds
     setTimeout(function() {
         bannerDiv.classList.add('hide'); // Hide the message
+        topDiv.classList.add('hide');
     }, 3000);
 });
