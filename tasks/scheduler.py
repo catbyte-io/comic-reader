690f38c1453986e9886366ef80cfb9d9e5edd922
catbyte-io <74119748+catbyte-io@users.noreply.github.com>
2025-11-16 19:34:59 -0700

    Create scheduler.py to run webscraping tasks periodically.

---
+++ tasks/scheduler.py
@@ -0,0 +1,19 @@
+from webscraper.webscraper import kcomic_scrape
+from webscraper.webscraper_en import ecomic_scrape
+
+import schedule
+import time
+import threading
+
+
+def run_scheduler():
+    schedule.every().wednesday.at("00:00").do(kcomic_scrape())
+    schedule.every().sunday.at("00:00").do(ecomic_scrape())
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+def start_scheduler():
+    scheduler_thread = threading.Thread(target=run_scheduler)
+    scheduler_thread.start()
