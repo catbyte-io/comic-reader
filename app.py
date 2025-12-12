48b0e76e23cbf4c3fe4a05328a7f499949885c50
catbyte-io <74119748+catbyte-io@users.noreply.github.com>
2025-12-12 16:48:55 -0700

    Try to fix directory creation issues.

--- app.py
+++ app.py
@@ -64,9 +64,11 @@ def load_user(user_id):
 
 
 def init_db():
+    db_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'db'))
+
     # Make the path for the directory if it doesn't exist yet
-    if not os.path.isdir('./db'):
-        os.mkdir('./db')
+    if not os.path.isdir(db_path):
+        os.mkdir(db_path)
 
     with sqlite3.connect('./db/webtoons.db') as conn:
         cursor = conn.cursor()
