405232852ca250ed35e7ee55eee00ddfa49c6932
catbyte-io <74119748+catbyte-io@users.noreply.github.com>
2025-12-17 15:09:45 -0700

    Disable gpu.

--- webscraper/webscraper.py
+++ webscraper/webscraper.py
@@ -38,6 +38,7 @@ def kcomic_scrape():
     options = Options()
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
+    options.add_argument('--disable-gpu')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--window-size=1920x1080')
 
--- webscraper/webscraper_en.py
+++ webscraper/webscraper_en.py
@@ -39,6 +39,7 @@ def ecomic_scrape():
     options = Options()
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
+    options.add_argument('--disable-gpu')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--window-size=1920x1080')
 
