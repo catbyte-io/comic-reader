bd43868c7bb47b9e2c9ea8225e3c88a96024057a
catbyte-io <catbyte.io@gmail.com>
2025-12-18 17:53:17 -0700

    Try using different headless flag.

--- webscraper/webscraper_en.py
+++ webscraper/webscraper_en.py
@@ -37,7 +37,7 @@ def ecomic_scrape():
     language = 'english'
 
     options = Options()
-    options.add_argument('--headless')
+    options.add_argument('--headless=new')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-gpu')
     options.add_argument('--disable-dev-shm-usage')
